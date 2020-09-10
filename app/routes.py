e35cc5d80b82a3226898cad0da385d0433a38df6
TurningIdeas Engineering <engg@turningideas.com>
2020-09-10 16:25:04 +0530

    Adding About Page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request,url_for
 
 @app.route('/')
 def home():
-	return render_template('base.html')
+	return render_template('home.html')
+
+@app.route('/about_afc')
+def about_afc():
+	return render_template('about.html')
