d54d742160a5c2d99a211aabd29157bdc0f35256
TurningIdeas Engineering <engg@turningideas.com>
2020-09-22 21:59:13 +0530

    UPDATED Added 4 pages

--- app/routes.py
+++ app/routes.py
@@ -8,3 +8,19 @@ def home():
 @app.route('/about_afc')
 def about_afc():
 	return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+	return render_template('projects.html')
+
+@app.route('/clients')
+def clients():
+	return render_template('clients.html')
+
+@app.route('/contact_us')
+def contact_us():
+	return render_template('contact_us.html', site_key=app.config["RECAPTCHA_SITE_KEY"])
+
+@app.route('/latest')
+def latest():
+	return render_template('latest.html')
